ff8d4b92201aba5c6b35d64bc7eb3c4f976ceecb
haiphan-eh <hai.phan@employmenthero.com>
2026-05-05 18:39:40 +0700

    fix: refactor Redis client usage and simplify lock acquisition logic

--- src/services/redis.service.ts
+++ src/services/redis.service.ts
@@ -1,10 +1,10 @@
-import { promisify } from 'node:util';
 import { InventoryService } from '@/services/index.js';
-import redis from 'redis';
-const redisClient = redis.createClient({});
+import { createClient } from 'redis';
 
-const pexpire = promisify(redisClient.pexpire).bind(redisClient);
-const setnxAsync = promisify(redisClient.setnx).bind(redisClient);
+const redisClient = createClient({});
+
+// Connect to Redis
+redisClient.connect().catch(console.error);
 
 export const acquireLock = async ({
   productId,
@@ -19,16 +19,16 @@ export const acquireLock = async ({
   const retryTimes = 10;
   const expireTime = 3000; // 3 seconds
   for (let i = 0; i < retryTimes; i++) {
-    const result = await setnxAsync();
+    const result = await redisClient.setNX(key, '1');
     console.log(' result:::', result);
-    if (result === 1) {
+    if (result) {
       const isReservation = await InventoryService.reservationInventory({
         productId,
         quantity,
         cartId,
       });
       if (isReservation.modifiedCount) {
-        await pexpire();
+        await redisClient.pExpire(key, expireTime);
         return key;
       }
       return null;
@@ -38,6 +38,5 @@ export const acquireLock = async ({
 };
 
 export const releaseLock = async (keyLock: string) => {
-  const delAsync = promisify(redisClient.del).bind(redisClient);
-  return await delAsync(keyLock);
+  return await redisClient.del(keyLock);
 };
